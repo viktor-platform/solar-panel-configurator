997e9d272b5a8a0b8608705c09b4162dbc9b7a03
dsommers <dsommers@viktor.ai>
2022-02-20 11:29:50 +0100

    made constants.py and deleted unnecessary file

--- app/configurator/controller.py
+++ app/configurator/controller.py
@@ -28,6 +28,9 @@ from viktor.views import PlotlyView
 from viktor.views import PlotlyResult
 
 from munch import Munch
+from .constants import type_dict
+from .constants import inverter_name_dict
+from .constants import module_name_dict
 from .parametrization import ConfiguratorParametrization
 from .pv_calculations import calculate_energy_generation
 
@@ -54,67 +57,6 @@ class Controller(ViktorController):
     @DataView("Data", duration_guess=1)  # only visible on "Step 2"
     def get_data_view(self, params, **kwargs):
         """Creates dataview for step 2 from the pv_calculation"""
-        type_dict = {
-            "California Energy Commission": "cecinverter",
-            "Sandia National Laboratories": "sandiainverter",
-        }
-        module_name_dict = {
-            "AstroPower APX-120": {
-                "name": "AstroPower APX-120 [ 2001]",
-                "price": 240.27,
-            },
-            "BP Solar SX160B": {
-                "name": "BP Solar SX160B [2005 (E)]",
-                "price": 395.45,
-            },
-            "Kyocera Solar PV110": {
-                "name": "Kyocera Solar PV110 [2003 (E)]",
-                "price": 242.68,
-            },
-            "Mitsubishi PV - MF185UD4": {
-                "name": "Mitsubishi PV-MF185UD4 [2006 (E)]",
-                "price": 154.20,
-            },
-            "Sanyo HIP - 200BE11": {
-                "name": "Sanyo HIP-200BE11 [2006 (E)]",
-                "price": 481.00,
-            },
-            "Sharp ND - L3E1U": {"name": "Sharp ND-L3E1U [2002 (E)]", "price": 675.00},
-            "Siemens Solar SP75(6V)": {
-                "name": "Siemens Solar SP75 (6V) [2003 (E)]",
-                "price": 50.00,
-            },
-            "Suntech STP200S - 18 - ub - 1 Module": {
-                "name": "Suntech STP200S-18-ub-1 Module [2009 (E)]",
-                "price": 480.00,
-            },
-        }
-        inverter_name_dict = {
-            "ABB: MICRO-0.3 Inverter": {  # First three CEC Inverters
-                "name": "ABB: MICRO-0.3-I-OUTD-US-240 [240V]",
-                "price": 173.19,
-            },
-            "Outback Power Tech. Inverter": {
-                "name": " OutBack Power Technologies - Inc : GS8048A [240V]",
-                "price": 3791.88,
-            },
-            "Hanwa Q-Cells Inverter": {
-                "name": " Hanwha Q CELLS America Inc : Q.HOME+ AC-G1-8.6 [240V]",
-                "price": 1877.47,
-            },
-            "Generac Power Systems Inverter": {  # Second three Sandia Inverters
-                "name": " Generac Power Systems: XVT076A03 [240V]",
-                "price": 3431.35,
-            },
-            "Delta Electronics Inverter": {
-                "name": " Delta Electronics: E4-TL-US(AC) [240V]",
-                "price": 3405.86,
-            },
-            "Chint Power Systems Inverter": {
-                "name": "Chint Power Systems America: CPS SCA5KTL-O US [240V]",
-                "price": 1616.45,
-            },
-        }
 
         energy_generation = calculate_energy_generation(
             params.step_1.point.lat,
@@ -169,69 +111,6 @@ class Controller(ViktorController):
     @PlotlyView("Plot", duration_guess=10)  # only visible on "Step 3"
     def get_plotly_view(self, params, **kwargs):
         """Shows the plot of the energy yield with break-even point"""
-
-        # re-do the energy calculation:
-        type_dict = {
-            "California Energy Commission": "cecinverter",
-            "Sandia National Laboratories": "sandiainverter",
-        }
-        module_name_dict = {
-            "AstroPower APX-120": {
-                "name": "AstroPower APX-120 [ 2001]",
-                "price": 240.27,
-            },
-            "BP Solar SX160B": {
-                "name": "BP Solar SX160B [2005 (E)]",
-                "price": 395.45,
-            },
-            "Kyocera Solar PV110": {
-                "name": "Kyocera Solar PV110 [2003 (E)]",
-                "price": 242.68,
-            },
-            "Mitsubishi PV - MF185UD4": {
-                "name": "Mitsubishi PV-MF185UD4 [2006 (E)]",
-                "price": 154.20,
-            },
-            "Sanyo HIP - 200BE11": {
-                "name": "Sanyo HIP-200BE11 [2006 (E)]",
-                "price": 481.00,
-            },
-            "Sharp ND - L3E1U": {"name": "Sharp ND-L3E1U [2002 (E)]", "price": 675.00},
-            "Siemens Solar SP75(6V)": {
-                "name": "Siemens Solar SP75 (6V) [2003 (E)]",
-                "price": 50.00,
-            },
-            "Suntech STP200S - 18 - ub - 1 Module": {
-                "name": "Suntech STP200S-18-ub-1 Module [2009 (E)]",
-                "price": 480.00,
-            },
-        }
-        inverter_name_dict = {
-            "ABB: MICRO-0.3 Inverter": {  # First three CEC Inverters
-                "name": "ABB: MICRO-0.3-I-OUTD-US-240 [240V]",
-                "price": 173.19,
-            },
-            "Outback Power Tech. Inverter": {
-                "name": " OutBack Power Technologies - Inc : GS8048A [240V]",
-                "price": 3791.88,
-            },
-            "Hanwa Q-Cells Inverter": {
-                "name": " Hanwha Q CELLS America Inc : Q.HOME+ AC-G1-8.6 [240V]",
-                "price": 1877.47,
-            },
-            "Generac Power Systems Inverter": {  # Second three Sandia Inverters
-                "name": " Generac Power Systems: XVT076A03 [240V]",
-                "price": 3431.35,
-            },
-            "Delta Electronics Inverter": {
-                "name": " Delta Electronics: E4-TL-US(AC) [240V]",
-                "price": 3405.86,
-            },
-            "Chint Power Systems Inverter": {
-                "name": "Chint Power Systems America: CPS SCA5KTL-O US [240V]",
-                "price": 1616.45,
-            },
-        }
 
         energy_generation = calculate_energy_generation(
             params.step_1.point.lat,
